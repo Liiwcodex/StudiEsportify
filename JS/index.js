5bcc4df37b59d9e39caece4fe2c98e21729671e7
Liiwcodex <wilfriedbietx@gmail.com>
2025-08-16 10:50:24 +0200

    Create index.js

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+
+
+  
+ // Fonction pour basculer l'affichage du menu
+ function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
